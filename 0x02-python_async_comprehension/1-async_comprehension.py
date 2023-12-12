936a2338c0f9a7e78bb95aeaf25aecc652110773
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-12-12 19:53:50 +0100

    Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Async Comprehension"""
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """uses async comprehension to get numbers from generator"""
+    return [i async for i in async_generator()]
