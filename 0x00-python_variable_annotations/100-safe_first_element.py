7adf4e8413228d38001ece0d4c1cf86c7ccff766
tha-orakkle <adegbiranayinoluwa.paul@yahoo.com>
2024-05-23 21:26:35 +0100

    documention included

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,7 +5,7 @@ from typing import Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """OK"""
+    """Returns the first element of a sequence"""
     if lst:
         return lst[0]
     else:
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -5,7 +5,13 @@ from typing import Any, Mapping, TypeVar, Union
 
 T = TypeVar('T')
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+
+def safely_get_value(
+        dct: Mapping,
+        key: Any,
+        default: Union[T, None] = None
+        ) -> Union[Any, T]:
+    """Returns the value of a key in a mapping"""
     if key in dct:
         return dct[key]
     else:
